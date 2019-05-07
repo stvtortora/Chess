ef1e9bda13418a56890a03264ee4879bc3f8b115
steven <stvtortora@gmail.com>
2019-05-06 23:43:09 -0400

    refactor pieces functions

--- Pieces.py
+++ Pieces.py
@@ -36,13 +36,19 @@ class Piece(object):
         self.board = board
         self.pos = pos
 
-    def is_safe_move(self, move):
-        board_clone = self.board.clone()
-        board_clone.move_piece(self.pos, move)
-        return not board_clone.in_check(self.color)
+    def can_move_to(self, move):
+        if self.board.on_board(move):
+            piece_found = self.board.piece_at(move)
+            return not piece_found or piece_found.color != self.color
+        return False
 
     def valid_moves(self):
-        return list(filter(lambda move: self.is_safe_move(move), self.moves()))
+        def is_valid_move(move):
+            board_clone = self.board.clone()
+            board_clone.move_piece(self.pos, move)
+            return not board_clone.in_check(self.color)
+
+        return list(filter(lambda move: is_valid_move(move), self.moves()))
 
 class NullPiece(Piece):
     def __init__(self, board, color=None, pos=None):
@@ -62,15 +68,9 @@ class Stepable(Piece):
             row_step, col_step = step
             return [start_row + row_step, start_col + col_step]
 
-        def is_valid_move(move):
-            if self.board.on_board(move):
-                piece_found = self.board.piece_at(move)
-                return not piece_found or piece_found.color != self.color
-            return False
-
         start_row, start_col = self.pos
         move_options = [move(step_option) for step_option in self.step_options]
-        return list(filter(lambda move: is_valid_move(move), move_options))
+        return list(filter(lambda move: self.can_move_to(move), move_options))
 
 class Slideable(Piece):
     def __init__(self, board, color, pos, move_dirs):
@@ -82,22 +82,18 @@ class Slideable(Piece):
         start_row, start_col = self.pos
 
         for move_dir in self.move_dirs:
+            move = self.pos
             row_dir, col_dir = move_dir
-            move = [start_row + row_dir, start_col + col_dir]
+            can_continue = True
 
-            piece_found = None
-            still_on_board = True
+            while can_continue:
+                prev_row, prev_col = move
+                move = [prev_row + row_dir, prev_col + col_dir]
 
-            while not piece_found and still_on_board:
-                piece_found = self.board.piece_at(move)
-
-                if not piece_found or piece_found.color != self.color:
-                    still_on_board = self.board.on_board(move)
-
-                    if still_on_board:
-                        result.append(move)
-                        prev_row, prev_col = move
-                        move = [prev_row + row_dir, prev_col + col_dir]
+                if self.can_move_to(move):
+                    result.append(move)
+                else:
+                    can_continue = False
 
         return result
 
@@ -143,24 +139,24 @@ class Pawn(Piece):
             return 1
         return -1
 
-    def at_start_pos(self):
-        return self.pos[0] == 1 if self.color == "white" else self.pos[0] == 6
-
-    def can_attack(self, pos):
-        piece_to_attack = self.board.piece_at(pos)
-        return piece_to_attack and piece_to_attack.color != self.color
-
-    def can_forward_step_to(self, pos):
-        return not self.board.piece_at(pos) and self.board.on_board(pos)
-
     def attack_moves(self):
+        def can_attack(pos):
+            piece_to_attack = self.board.piece_at(pos)
+            return piece_to_attack and piece_to_attack.color != self.color
+
         row, col = self.pos
         attack_pos = [[row + self.forward_step(), col - 1], [row + self.forward_step(), col + 1]]
-        return list(filter(lambda pos: self.can_attack(pos), attack_pos))
+        return list(filter(lambda pos: can_attack(pos), attack_pos))
 
     def forward_moves(self):
+        def at_start_pos():
+            return self.pos[0] == 1 if self.color == "white" else self.pos[0] == 6
+
+        def can_forward_step_to(pos):
+            return not self.board.piece_at(pos) and self.board.on_board(pos)
+
         row, col = self.pos
         forward_pos = [[row + self.forward_step(), col]]
-        if self.at_start_pos():
+        if at_start_pos():
             forward_pos.append([row + self.forward_step() * 2, col])
-        return list(filter(lambda pos: self.can_forward_step_to(pos), forward_pos))
+        return list(filter(lambda pos: can_forward_step_to(pos), forward_pos))
